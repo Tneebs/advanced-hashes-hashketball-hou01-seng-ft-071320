457258a1888018746e600291e7c881df493369e2
Ted Neben <tedneben@gmail.com>
2020-07-02 20:09:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -186,10 +186,6 @@ end
 def player_stats(player_name)
   game_hash.each do |team, team_attrbiutes|
     team_attributes[:players].each do |player|
-      if player == player_name
-        all_stats = stats
-      end
     end
   end
-  all_stats
 end
